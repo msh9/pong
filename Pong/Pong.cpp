cd2074481b73adcfb96f98d55b3e47230127ad22
Michael Hughes <michael@mihughes.com>
2016-05-28 09:13:02 -0600

    Implement ever-so-slightly less naive state update frequency control; it now runs @ ~30Hz

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -1,10 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include "PongGame.h"
+#include <chrono>
+#include <ratio>
 using namespace std;
 int main() {
 	sf::RenderWindow window(sf::VideoMode(600, 600), "Pong");
 	PongGame pong;
-	window.setFramerateLimit(60); 
+
+	/*
+	Game state is updated at ~30Hz
+	Draw frequency is however fast it goes
+	*/
+	const int stateUpdateHertz = 30;
+	const chrono::duration <int, ratio<1, stateUpdateHertz>> oneThirtySecond(1);
+	auto nextStateUpdateTime = chrono::steady_clock::now() + oneThirtySecond;
+	
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -15,7 +25,13 @@ int main() {
 			pong.update(event);
 
 		}
-		pong.updateGameState();
+		//Naive implementation, we assume the outer loop executes fast enough
+		//that it's reasonable to just update state as soon as we pass the marker
+		//for the next state update
+		if (chrono::steady_clock::now() > nextStateUpdateTime) {
+			pong.updateGameState();
+			nextStateUpdateTime = chrono::steady_clock::now() + oneThirtySecond;
+		}
 		window.clear(sf::Color::Black);
 		pong.render(&window);
 		window.display();
